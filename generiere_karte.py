03e2f8d96c67204a5ebf435ea2ef5e998cbad5f3
Geduldige Birne <106613623+Thadhaus@users.noreply.github.com>
2022-05-31 16:08:38 +0000

    Initial commit

---
+++ generiere_karte.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# (c) 2022 r.haerter@wut.de
+#
+# Dies ist ein kleiner Helfer, um die Karte aus dem selbst
+# geschriebenen Textadventure als Bild darzustellen.
+# Verwende dazu graphviz, das gibt es für jede Plattform.
+
+# mit Farbe wird es bunter - siehe https://pypi.org/project/colorama/
+import colorama
+
+colorama.init(autoreset=True)
+from colorama import Fore, Back, Style
+
+# Feste Liste von idealerweise eindeutigen, nicht zu kurzen Namen für die Dictionaries
+# Diese Liste muss man vielleicht an sein eigenes Textadventure anpassen!
+dicts = [
+    'north', 'east', 'south', 'west', 'upward', 'upstairs', 'downward',
+    'downstairs', 'teleport'
+]
+
+
+# Zerlege die gefundenen Dictionaries
+# ToDo: Man könnte die Richtung nutzen, um die Pfeile in den
+#       generierten Graphen zu beschriften
+def parse_direction(zeile):
+    try:
+        richtung, rest = zeile.split('=')
+    except:
+        #print ("Keine Zuweisung gefunden, hier ist wohl kein Dictionary ...")
+        return "fail"
+    #print("Ri: _{}_ - Re: _{}_".format(richtung.strip(),rest.strip()[1:-1]))
+    # Formatierungsartefakte entfernen
+    neu = rest.replace("'", "")
+    rest = neu.replace(" ", "")
+    listenende = -1
+    if rest.rstrip()[-1] == '\\':
+        print(
+            f"{Fore.RED}Fortsetzungszeichen erkannt. Deine Karte wird nicht vollständig sein."
+        )
+        listenende = -2
+    # Das Ergebnis in eine Liste zurückverwandeln
+    verbindungsliste = list(rest.strip()[1:listenende].split(','))
+    return (verbindungsliste)
+
+
+def erzeuge_graph(vliste):
+    ergebnis = ""
+    for element in vliste:
+        von, nach = element.split(':')
+        if nach == 'None':
+            pass
+        else:
+            result = "{}->{}\n".format(von, nach)
+            ergebnis += result
+    return (ergebnis)
+
+
+def schreibe_graphviz(graph, gvname):
+    ergebnis = open(gvname, "w")
+    result_head = '''
+digraph Ravenswood {
+layout=neato
+'''
+    result_tail = '''
+overlap=false
+label="Your Textadventure Map layed out by Graphviz "
+fontsize=12;
+}
+    '''
+    ergebnis.write(result_head)
+    ergebnis.write(graph)
+    ergebnis.write(result_tail)
+    ergebnis.flush()
+    ergebnis.close()
+
+
+def hauptprogramm(fname):
+    pyname = fname
+    filename, ext = pyname.split('.', 1)  # nur ein Punkt im Namen, bitte
+    datei = open(pyname, 'r')
+    gvname = filename + '.gv'
+
+    alle_verbindungen = []
+    fortsetzung = False
+    komplett = False
+    zeile = ""
+    for line in datei.readlines():
+        if line.startswith('#'):
+            pass
+        elif line.startswith('allowed_commands'):
+            pass
+        elif line.startswith('compass'):
+            pass
+        elif fortsetzung:
+            zeile += line.strip()
+            if '}' in line:
+                fortsetzung = False
+                komplett = True
+        elif any(richtung in line for richtung in dicts):
+            if '=' in line:
+                zeile += line.strip()
+                fortsetzung = True
+        if komplett == True:
+            #print(f"Zeile ist {zeile}\n")
+            result = parse_direction(zeile)
+            zeile = ""
+            if result == 'fail':
+                pass
+            else:
+                alle_verbindungen += result
+            komplett = False
+
+    graph = erzeuge_graph(alle_verbindungen)
+    schreibe_graphviz(graph, gvname)
+    print("Fertig. Die Datei {} wurde erfolgreich erstellt".format(gvname))
+    print(
+        "Um diese Datei in ein Bild umzuwandeln, brauchst du Graphviz: https://graphviz.org/"
+    )
+    print(
+        f"Damit kann jetzt mit dem Befehl 'neato -Tpng {gvname} > {filename}.png' ein Hausplan erstellt werden."
+    )
+
+
+if __name__ == '__main__':
+    hauptprogramm()
---
+++ main.py
@@ -0,0 +1,303 @@
+# https://www.helloworld.cc - Heft 1 - Seite 52
+# Scary cave game -- Original Version CC BY-NC-SA 3.0
+# Diese modifizierte Version (C) 2022 Roland Härter r.haerter@wut.de
+
+# mit Farbe wird es bunter - siehe https://pypi.org/project/colorama/
+import colorama
+
+colorama.init(autoreset=True)
+from colorama import Fore, Back, Style
+
+# Mit Zufall wird es spannender
+import time, random
+
+random.seed(time.time())
+
+import sys
+
+# So funktioniert das Spiel: Alle Räume sind in Richtungs-Dictionaries.
+# Diese Dictionaries 'north', 'south',... enthalten Schlüssel-Wert-Paare.
+# Schlüssel ist der aktuelle Raum, Wert der Zielraum, in den ich gelange.
+# Ein Wert 'None' bedeutet, das ich in dieser Richtung nirgendwo hin komme.
+# Mit den 'go'-Kommandos im compass kann ich durch die Räume laufen
+# Alle Befehlswörter müssen auch in allowed_commands stehen.
+north = {
+    'R0': None,
+    'R1': None,
+    'R2': 'R0',
+    'R3': 'R1',
+    'R4': None,
+    'R5': None,
+    'R6': 'R7',
+    'R7': None,
+    'R8': None,
+    'R9': None,
+    'R10': 'R9',
+    'R11': 'R16',
+    'R12': 'R11',
+    'R13': None,
+    'R14': None,
+    'R15': 'R14',
+    'R16': 'R10',
+    'R17': None,
+    'R18': None,
+    'R19': None,
+    'R20': 'R7',
+    'R21': 'R12'
+}
+south = {
+    'R0': 'R2',
+    'R1': 'R3',
+    'R2': None,
+    'R3': 'R9',
+    'R4': None,
+    'R5': None,
+    'R6': None,
+    'R7': 'R8',
+    'R8': None,
+    'R9': 'R10',
+    'R10': 'R16',
+    'R11': 'R12',
+    'R12': 'R21',
+    'R13': None,
+    'R14': 'R15',
+    'R15': None,
+    'R16': 'R11',
+    'R17': None,
+    'R18': None,
+    'R19': None,
+    'R20': None,
+    'R21': None
+}
+east = {
+    'R0': 'R1',
+    'R1': None,
+    'R2': None,
+    'R3': None,
+    'R4': 'R5',
+    'R5': None,
+    'R6': None,
+    'R7': None,
+    'R8': None,
+    'R9': None,
+    'R10': None,
+    'R11': None,
+    'R12': None,
+    'R13': 'R16',
+    'R14': None,
+    'R15': 'R17',
+    'R16': 'R15',
+    'R17': 'R18',
+    'R18': 'R19',
+    'R19': None,
+    'R20': None,
+    'R21': None
+}
+west = {
+    'R0': 'R4',
+    'R1': 'R0',
+    'R2': None,
+    'R3': None,
+    'R4': None,
+    'R5': None,
+    'R6': None,
+    'R7': None,
+    'R8': None,
+    'R9': None,
+    'R10': None,
+    'R11': None,
+    'R12': None,
+    'R13': None,
+    'R14': 'R13',
+    'R15': 'R16',
+    'R16': 'R13',
+    'R17': 'R15',
+    'R18': 'R17',
+    'R19': 'R18',
+    'R20': None,
+    'R21': None
+}
+downward = {
+    'R0': None,
+    'R1': None,
+    'R2': None,
+    'R3': None,
+    'R4': None,
+    'R5': 'R6',
+    'R6': None,
+    'R7': None,
+    'R8': None,
+    'R9': None,
+    'R10': None,
+    'R11': None,
+    'R12': None,
+    'R13': None,
+    'R14': None,
+    'R15': None,
+    'R16': None,
+    'R17': None,
+    'R18': None,
+    'R19': 'R20',
+    'R20': None,
+    'R21': None
+}
+upstairs = {
+    'R0': None,
+    'R1': None,
+    'R2': None,
+    'R3': None,
+    'R4': None,
+    'R5': None,
+    'R6': None,
+    'R7': None,
+    'R8': 'R2',
+    'R9': None,
+    'R10': None,
+    'R11': None,
+    'R12': None,
+    'R13': None,
+    'R14': None,
+    'R15': None,
+    'R16': None,
+    'R17': None,
+    'R18': None,
+    'R19': None,
+    'R20': None,
+    'R21': None
+}
+# 'compass' bildet die Richtungs-Befehle auf die Richtungs-Dictionaries ab
+compass = {
+    'go north': north,
+    'w': north,
+    'go south': south,
+    's': south,
+    'go east': east,
+    'd': east,
+    'go west': west,
+    'a': west,
+    'go up': upstairs,
+    'up': upstairs,
+    'go down': downward,
+    'dn': downward
+}
+# Alle Befehlswörter des Spiels müssen in 'allowed_commands' stehen
+allowed_commands = [
+    'go north', 'go south', 'go east', 'go west', 'go up', 'go down', 'help',
+    'quit', 'map', 'w', 'a', 's', 'd', 'up', 'dn'
+]
+
+# Jeder Raum hat eine Beschreibung. Die Beschreibung macht viel vom Spiel aus.
+description = {
+    'R0': 'You are in the kitchen. Seems to be abandonned.',
+    'R1': 'You are in the living room. An old armor stands in one corner.',
+    'R2': 'You are in a pantry. It is cold in here.',
+    'R3': 'Hallway. Here is the exit from this house.',
+    'R4': 'The beautiful garden with an apple tree.',
+    'R5': 'A small cottage in the garden.',
+    'R6': 'The cellar below the cottage.',
+    'R7': 'A tunnel with low ceiling.',
+    'R8': 'A cellar with shelves full of old onions.',
+    'R9': 'A square in front of the house strewn with gravel.',
+    'R10': 'A path strewn with gravel in front of the house.',
+    'R11': 'A path strewn with gravel in front of the house.',
+    'R12': 'A path strewn with gravel towards the road.',
+    'R13': 'A gravel strewn path through the park.',
+    'R14': 'A gravel strewn path through the park.',
+    'R15': 'A gravel strewn path through the park.',
+    'R16': 'A crossing of four paths in the park.',
+    'R17': 'A small summer house with a blue door.',
+    'R18': 'The kitchen living in the summer house.',
+    'R19': 'The storage room in the summer house.',
+    'R20': 'There is a floor hatch under the carpet in the storage room.',
+    'R21': 'The gates are closed and locked.'
+}
+
+# to play different sound indoor and outdoor
+indoor_rooms = [
+    'R0',
+    'R1',
+    'R2',
+    'R3',
+    'R5',
+    'R6',
+    'R7',
+    'R8',
+    'R17',
+    'R18',
+    'R19',
+]
+
+indoor_sounds = [
+    'It crunches and scratches in the wall. ',
+    'It trips like rats under the floorboards.',
+    'You hear a dripping water tap.',
+    'You feel an opressive silence in the house.',
+    'There is a whispering murmur around you.',
+]
+
+outdoor_sounds = [
+  "The birds are singing loudly.",
+  "A dog is barking in the distance."
+]
+
+def random_sounds(room):
+    zufall = random.randint(0, 100)
+    if zufall < 11:
+        if room in indoor_rooms:
+            text = indoor_sounds[random.randrange(len(indoor_sounds))]
+            print(text)
+        else:
+            text = outdoor_sounds[random.randrange(len(outdoor_sounds))]
+            print(text)
+
+
+def hilfe():
+    print(Fore.CYAN + '\nYou may use the following commands:')
+    for befehl in allowed_commands:
+        if befehl == 'tp' or befehl == 'jump':
+            pass
+        else:
+            print(Fore.BLUE + Style.BRIGHT + f"'{befehl}' ", end='')
+    print('\n')
+
+
+# Hier wird Start und Ziel festgelegt
+current_room = 'R0'
+final_room = 'R99'  # no final room so far ...
+# Begrüßung
+print(Fore.MAGENTA + '	*** Welcome to Ravenswood Manor ***')
+hilfe()
+# Ab hier folgt der Code, um im Spiel zu agieren
+command = ''
+# Das Spiel läuft in einer Endlos-Schleife
+while (current_room is not None):
+    # Die Beschreibung des aktuellen Raums ausgeben
+    print(Fore.YELLOW + description[current_room])
+    random_sounds(current_room)
+    # Den Spieler nach seinem nächsten Kommando fragen
+    command = input(Fore.GREEN + 'What do you want to do? ' +
+                    Fore.RESET).lower()
+    # Alle unbekannten Eingaben ignorieren
+    while command not in allowed_commands:
+        command = input(Fore.RED + 'No such command. ' + Fore.GREEN +
+                        'What do you want to do? ' + Fore.RESET).lower()
+    # alle Nicht-Richtungen müssen zuerst abgefangen werden
+    if command == 'help':
+        hilfe()
+    elif command == 'quit':
+        current_room = None  # Ohne Schmuck und ohne Sicherheitsfrage
+    elif command == 'map':
+        import generiere_karte as map
+        map.hauptprogramm(sys.argv[0])  # der Name dieses Skripts als Parameter
+    # Gibt es einen Weg in die gefragte Richtung?
+    elif compass[command][current_room] is not None:
+        # Wenn es einen Weg gibt, gehe dorthin
+        current_room = compass[command][current_room]
+        # Wurde das Spielziel erreicht?
+        if current_room == final_room:
+            print(Fore.YELLOW + description[current_room])
+            print(Fore.MAGENTA + 'You found the final room. Game Over.' +
+                  Fore.RESET)
+            current_room = None  # Die Abbruch-Bedingung für die Endlos-Schleife setzen
+    else:
+        print(Fore.RED + 'There is no path in that direction. ', end='')
